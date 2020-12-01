d316e8cfa94915385af6e983e4a7e4612190ddfc
aqrl-alessandro <alessandrobarreto1201@gmail.com>
2020-11-30 21:10:50 -0300

    ternary ex

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useEffect } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
+  const [value, setValue] = useState("");
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  const changeValue = () => {
+    value === '' ? setValue('banana') : setValue('')
+  };
+  
+
+  return (
+    <section>
+      <h1>{value && 'texto'}</h1>
+      <h2>{"texto" || value}</h2>
+      <button className="btn" onClick={changeValue}>
+        change value
+      </button>
+    </section>
+  );
 };
 
 export default ShortCircuit;
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  const [hide, setHide] = useState(false);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+  }, []);
+
+  return (
+    <section>
+      <h1>{hide ? "Escondeu!!!" : "Window size"}</h1>
+      <h2>{hide || size + "PX"}</h2>
+      <h3>{hide && "ops... click again"}</h3>
+      <button className="btn" onClick={() => setHide(!hide)}>
+        {hide ? "show" : "hide"}
+      </button>
+    </section>
+  );
 };
 
 export default ShowHide;
